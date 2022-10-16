aa168487aa44dfc99b3e4c26bad1aff297e4ef1f
jeremyoutinen <jeremyoutinen@gmail.com>
2022-10-16 09:59:38 -0500

    Add support for search results with no memberships
    It's apparently possible to have an account with no platform (Xbox, Steam, PSN, etc.). Modify styling of the search result if there is no platform.

--- src/js/views/previewView.js
+++ src/js/views/previewView.js
@@ -6,16 +6,23 @@ class PreviewView extends View {
   _generateMarkup() {
     const id = window.location.hash.slice(1);
 
-    return `
-    <li class="search-result">
-      <p class="profile-name">${this._data.bungieGlobalDisplayName}#${this._data.bungieGlobalDisplayNameCode}</p>
-      <img
-        src="https://www.bungie.net/${this._data.destinyMemberships[0].iconPath}"
-        alt="Platform Logo"
-        class="search-result-platform-img"
-      />
-  </li>
-`;
+    if (this._data.destinyMemberships.length) {
+      return `
+      <li class="search-result">
+        <p class="profile-name">${this._data.bungieGlobalDisplayName}#${this._data.bungieGlobalDisplayNameCode}</p>
+        <img
+          src="https://www.bungie.net/${this._data.destinyMemberships[0].iconPath}"
+          alt="Platform Logo"
+          class="search-result-platform-img"
+        />
+      </li>`;
+    } else {
+      return `
+      <li class="search-result-no-platform">
+        <p class="profile-name-no-platform">${this._data.bungieGlobalDisplayName}#${this._data.bungieGlobalDisplayNameCode}</p>
+        <i class="fa-solid fa-circle-xmark search-result-platform-img-no-platform"></i>
+      </li>`;
+    }
   }
 }
 
