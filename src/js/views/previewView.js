23b3b9b737d03c1e3777fddc83d0bfdf22329036
jeremyoutinen <jeremyoutinen@gmail.com>
2022-10-16 22:53:58 -0500

    Refactor search results to be anchor tags.
    Use membership ID as the href when the user selects a profile.

--- src/js/views/previewView.js
+++ src/js/views/previewView.js
@@ -5,13 +5,12 @@ class PreviewView extends View {
 
   _generateMarkup() {
     if (this._data.destinyMemberships.length) {
+      this._data.id = this._data.destinyMemberships[0].membershipId;
       return `
       <li class="search-result">
-        <p class="profile-name">${
-          this._data.bungieGlobalDisplayName
-        }#${this._data.bungieGlobalDisplayNameCode
-        .toString()
-        .padStart(4, '0')}</p>
+        <a class="profile-name" href="#${this._data.id}">${
+        this._data.bungieGlobalDisplayName
+      }#${this._data.bungieGlobalDisplayNameCode.toString().padStart(4, '0')}
         <img
           src="https://www.bungie.net/${
             this._data.destinyMemberships[0].iconPath
@@ -19,16 +18,16 @@ class PreviewView extends View {
           alt="Platform Logo"
           class="search-result-platform-img"
         />
+        </a>
       </li>`;
     } else {
       return `
       <li class="search-result-no-platform">
-        <p class="profile-name-no-platform">${
+        <a class="profile-name-no-platform" href="#">${
           this._data.bungieGlobalDisplayName
-        }#${this._data.bungieGlobalDisplayNameCode
-        .toString()
-        .padStart(4, '0')}</p>
+        }#${this._data.bungieGlobalDisplayNameCode.toString().padStart(4, '0')}
         <i class="fa-solid fa-circle-xmark search-result-platform-img-no-platform"></i>
+        </a>
       </li>`;
     }
   }
